641028deb195f7ff7a491c2336ef3377fb30af88
Tochukwu Nwachukwu <tnwachukwu@realmdigital.co.za>
2019-03-11 04:33:08 +0200

    Debugging and Diagnistics

---
+++ Chapter15/Chapter15/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 10;
+            if(age == 15)
+            {
+                Console.WriteLine("Control should be here");
+            }
+            else
+            {
+                Console.WriteLine("Control should NOT be here");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
