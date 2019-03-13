ebad10eb71b8ba58f802c57c44071ae087e202a1
Tochukwu Nwachukwu <tnwachukwu@realmdigital.co.za>
2019-03-13 05:06:26 +0200

    Regular exppression - System.Text.RegularExpressions

---
+++ Chapter9B/Chapter9B/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Chapter9B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            startValidation:
+            Console.Write("Enter a valid Email Address: ");
+            string email = Console.ReadLine();
+            string pattern = @"\w+@\w+.\w{2,4}";
+            bool matchEmail = Regex.IsMatch(email, pattern);
+            if (matchEmail)
+            {
+                Console.WriteLine($"{email} is a valid email address");
+            }
+            else
+            {
+                Console.WriteLine($"{email} is invalid email address");
+            }
+            goto startValidation;
+
+        }
+    }
+}
