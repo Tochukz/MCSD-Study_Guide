b8e78fdd36f4049ff99c372e032569226688d5cd
Tochukwu Nwachukwu <tnwachukwu@realmdigital.co.za>
2019-03-13 05:03:05 +0200

    Exception handling - ArgumentNullException, DivideByZeroException, Exception

---
+++ Chapter9/Chapter9/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                
+                Divide(5, 4, null);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"Invalid Argument: {ex.Message}");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"Invalid input: {ex.Message}");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"General exception: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Program End");
+            }
+
+        }
+        static void Divide(int x, int y, string s)
+        {
+            if(s == null)
+            {
+                throw new ArgumentNullException();
+            }
+            Console.WriteLine($"{s} {x / y}");
+        }
+
+    }
+}
